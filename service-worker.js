f3c14825fbe93b2b3649957ee9b0bbb45bc91063
Froidelecservice <robert.auray.fes@gmail.com>
2026-01-04 17:40:36 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,28 @@
-const CACHE_NAME = "stock-fesfcm-cache-v1";
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json"
+const CACHE_NAME = "stock-fes-fcm-v1";
+const ASSETS = [
+  "index.html",
+  "manifest.json",
+  "service-worker.js",
+  "icon-192.png",
+  "icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
-
 });
-
-
